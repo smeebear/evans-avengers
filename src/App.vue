cd9751e2fab32b4b6cbb538f0ed8156050a50d57
dsoreal <davidsoreal33@gmail.com>
2021-05-28 15:47:54 -0500

    create base navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <navbar class=""/>
+  <router-view/>  
 </template>
+
+<script>
+import navbar from './components/navbar.vue'
+
+export default {
+  components: {
+    navbar
+  }
+}
+</script>
+
 
 <style>
 #app {
---
+++ src/components/navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="nav bg-dark text-white h-12">
+      <ul class='cursor-pointer '>
+          <li class="inline hover:bg-space">Home</li>
+          |
+          <li class="inline hover:bg-space">About</li>
+          |
+          <li class="inline hover:bg-space">Whatever</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
