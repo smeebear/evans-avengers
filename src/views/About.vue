5d843aa3f823f512fc45e88b00e08c3c12d240ad
smeebear <soheron@gamil.com>
2021-05-28 16:18:11 -0500

    Added list

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,61 @@
 <template>
-  <div class="about bg-blue-300">
-    <h1>This is an about page</h1>
+  <div class="about">
+    <h1 class="text-6xl">Meet the team</h1>
+    <ul>
+      <li>
+        <h3 class="text-3xl">Piddles The Bard The Bard</h3>
+        <p class="text-lg">Stupid boy</p>
+      </li>
+      <li>
+        <h3 class="text-3xl">Norrin Vengir</h3>
+        <p class="text-lg">Toilet cleaner</p>
+      </li>
+      <li>
+        <h3 class="text-3xl">Saphira Rivendale</h3>
+        <p class="text-lg">Tries to fly. Can't.</p>
+      </li>
+      <li>
+        <h3 class="text-3xl">Corvus Blackwell</h3>
+        <p class="text-lg">BOOOOOOONES</p>
+      </li>
+      <li>
+        <h3 class="text-3xl">Sandro</h3>
+        <p class="text-lg">Sneaky sneaky stabby stabby</p>
+      </li>
+      <li>
+        <h3 class="text-3xl">Torgus Hammerfell</h3>
+        <p class="text-lg">Rolling to see if there are any materials</p>
+      </li>
+      <li>
+        <h3 class="text-3xl">Varan</h3>
+        <p class="text-lg">POOF. Mage Armor.</p>
+      </li>
+      <li>
+        <h3 class="text-3xl">Vodran Ashwood</h3>
+        <p class="text-lg">My spit hurts.</p>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  /* display: inline-block; */
+  margin: 0 10px;
+}
+
+</style>
